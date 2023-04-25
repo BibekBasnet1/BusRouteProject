82bf87c39cae108af830ff339d181afeb73aa795
Bibek Basnet <bbkbsnyt@gmail.com>
2023-04-25 15:58:24 +0545

    made Changes in hero section and wrote footersection as well as made changes to css files

---
+++ views/about_section.php
@@ -0,0 +1,29 @@
+<section id="about">
+    <h2 class="about-us-title" style="margin-bottom: 3em;text-align: center">About Us</h2>
+    <div class="container-student">
+        <div class="img-file">
+            <img src="../images/50426-removebg-preview.png" class="illustration-about" alt="">
+        </div>
+        <div class="description">
+            <h3 style="margin-bottom:1rem; margin-top: 4rem;">Student <img src="../images/graduated.png" width="20px" alt=""></h3>
+            <p>Student will be able to get easily get their
+                scheduled pickup after they sign in. They
+                are required to sign in with a valid location
+                they live in for correct bus location pick up
+            </p>
+        </div>
+    </div>
+    <div class="container-admin">
+        <div class="img-file container-admin-img">
+            <img src="../images/5437683.jpg" class="illustration-about" alt="">
+        </div>
+        <div class="description container-admin-des">
+            <h3 style="margin-bottom:1rem; margin-top: 4rem;">Teacher <img src="../images/teacher.png" width="20px" alt=""></h3>
+            <p>Admin will be able to manage the pickUps through customized dashboard.
+                Admin will be able to see the details of the students and as well as
+                drivers. Admin will have every access rights and will be able easily manage
+                the students as well as provide scheduled pickUps.
+            </p>
+        </div>
+    </div>
+</section>
--- views/index.php
+++ views/index.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="main.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <title>Bus Route</title>
 </head>
 <body>
@@ -18,42 +19,14 @@
         <main class="main-content">
             <?php include "hero_section.php"?>
             <!-- start of the about section -->
-            <section id="about">
-                <h2 class="about-us-title" style="text-align: center;">About Us</h2>
-                <div class="container-student">
-                    <div class="img-file">
-                        <img src="../images/50426-removebg-preview.png" class="illustration-about" alt="">
-                    </div>
-                    <div class="description">
-                        <h3 style="margin-bottom:1rem; margin-top: 4rem;">Student <img src="../images/graduated.png"
-                                width="20px" alt=""></h3>
-                        <p>Student will able to get easily get their
-                            schdeuled pick up after they sign in. They
-                            are required to sign in with a valid location
-                            they live in for correct bus location pick up
-                        </p>
-                    </div>
-                </div>
-                <div class="container-admin">
-                    <div class="img-file container-admin-img">
-                        <img src="../images/5437683.jpg" class="illustration-about" alt="">
-                    </div>
-                    <div class="description container-admin-des">
-                        <h3 style="margin-bottom:1rem; margin-top: 4rem;">Teacher <img src="../images/teacher.png"
-                                width="20px" alt=""></h3>
-                        <p>Admin will be able to manage the pickUps through customized dashboard.
-                            Admin will be able to see the details of the students and as well as
-                            drivers. Admin will have every access rights and will be able easily manage
-                            the students as well as provide schdelued pickUps.
-                        </p>
-                    </div>
-                </div>
-            </section>
+             <?php
+                include "about_section.php";
+             ?>
             <!-- end of the about section -->
 
             <!-- start of the feature section -->
-            <section class="features">
-                <h2 style="text-align: center;">How to get started <br> With Our Application</h2>
+            <section class="features" id="Features">
+                <h2 style="text-align: center;margin-top: 4em;">How to get started <br> With Our Application</h2>
 
                 <div class="grid-container">
                     <div class="grid-child child-1">
@@ -62,14 +35,15 @@
                                 <img src="../images/sign-up-form-css-html.png" alt="">
                             </div>
 
-                            <figcaption class="sign-up-form"><span>1</span> <h2>Create an Acc</h2></figcaption>
+<!--                            <figcaption class="sign-up-form"><span>1</span> <h2>Create an Acc</h2></figcaption>-->
                         </figure>
                     </div>
                     <div class="grid-child child-2">
-
+                        <span>1. &nbsp; Create an Acc</span>
                     </div>
                     <div class="grid-child child-3">
-
+                        <span>2. &nbsp; Manage Using dashboard </span>
+                        <div class="connecting-line"></div>
                     </div>
                     <div class="grid-child child-4">
                         <figure>
@@ -77,14 +51,20 @@
                                 <img src="../images/dashboard_image-removebg-preview.png" alt="">
                             </div>
 
-                            <figcaption class="dashboard-caption"><span>2</span> <h2>Manage Using Dashboard</h2></figcaption>
+<!--                            <figcaption class="dashboard-caption"><span>2</span> <h2>Manage Using Dashboard</h2></figcaption>-->
                         </figure>
                     </div>
                     <div class="grid-child child-5">
+                        <figure>
+                            <div class="img-container-sign-up">
+                                <img src="../images/web-3120321_640.png" alt="">
+                            </div>
 
+<!--                            <figcaption class="dashboard-caption"><span>3</span><h2>Get your Location in latitude and longitude</h2></figcaption>-->
+                        </figure>
                     </div>
                     <div class="grid-child child-6">
-
+                        <span>3. &nbsp; Get Your location in latitude and longitude</span>
                     </div>
                 </div>
             </section>
@@ -92,15 +72,55 @@
             <!-- end of the feature section -->
 
             <!-- start of the contact section -->
-            <section id="contact">
-
-            </section>
+<!--            <section id="contact">-->
+<!--                    -->
+<!--            </section>-->
             <!-- end of the contact section -->
         </main>
 
         <!-- this is the end of te main section -->
-        <footer id="footer-section">
-
+        <footer class="footer">
+            <div class="container-footer">
+                <div class="row">
+                    <div class="footer-col">
+                        <h4>company</h4>
+                        <ul>
+                            <li><a href="#">about us</a></li>
+                            <li><a href="#">our services</a></li>
+                            <li><a href="#">privacy policy</a></li>
+                            <li><a href="#">affiliate program</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>get help</h4>
+                        <ul>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">shipping</a></li>
+                            <li><a href="#">returns</a></li>
+                            <li><a href="#">order status</a></li>
+                            <li><a href="#">payment options</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>online shop</h4>
+                        <ul>
+                            <li><a href="#">watch</a></li>
+                            <li><a href="#">bag</a></li>
+                            <li><a href="#">shoes</a></li>
+                            <li><a href="#">dress</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>follow us</h4>
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     </div>
     <script src="index.js"></script>
